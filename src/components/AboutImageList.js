e5fa836ee0526dddea29b686686709a6fb312d15
Jason Mize <jasonemize@gmail.com>
2020-06-27 14:10:06 -0400

    fix for about page

---
+++ src/components/AboutImageList.js
@@ -0,0 +1,8 @@
+export const JasonImages = [
+  {
+    url: require('../../public/images/about/jasonProfile_tux.jpg'),
+    tinyUrl: require('../../public/images/about/jasonProfile_tux.jpg'),
+    mediumUrl: require('../../public/images/about/jasonProfile_tux.jpg'),
+    caption: 'A Fine Looking Goat'
+  }
+]
